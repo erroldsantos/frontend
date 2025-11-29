438fe9eb1384b6738477bb63ceeaa44f1be3f153
Siezz <erroldsantos25@gmail.com>
2025-11-29 18:08:48 +0800

    Update withCredentials to true for CORS

--- src/stores/api.js
+++ src/stores/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json'
   },
-  withCredentials: false
+  withCredentials: true
 })
 
 // Add response interceptor for consistent error handling
